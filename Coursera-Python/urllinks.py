50e64c3cd0798691c9c4f978177468c66108c97a
Munigala Venkatesh <109991223+venkatmv41@users.noreply.github.com>
2022-07-29 13:59:08 +0530

    Add files via upload
    Following Links in Python,
    Following Links in HTML Using BeautifulSoup

---
+++ Coursera-Python/urllinks.py
@@ -0,0 +1,30 @@
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+import re
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+count = int(input(" enter count :  "))
+position = int(input(" enter pos:  "))
+url = input('Enter - ')
+
+while(count>0):
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    # Retrieve all of the anchor tags
+    tags = soup('a')
+    array = []
+    for tag in tags:
+        x = tag.get('href', None)
+        array.append(x)
+    z = array[position-1]
+    url =z
+    print(url)
+    count-=1
+
+
+
