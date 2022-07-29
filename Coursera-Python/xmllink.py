283f6c25a89e5de31d9d08ac379200642cd996a8
Munigala Venkatesh <109991223+venkatmv41@users.noreply.github.com>
2022-07-30 03:53:11 +0530

    Add files via upload
    extracting data from xml through parsing in python

---
+++ Coursera-Python/xmllink.py
@@ -0,0 +1,28 @@
+import urllib.request, urllib.parse, urllib.error
+import xml.etree.ElementTree as ET
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter location: ')
+x = 1
+while(x>0):
+    if len(url) < 1: break
+    parms = dict()
+    parms['url'] = url
+    uh = urllib.request.urlopen(url, context=ctx)
+    data = uh.read()
+    z = data.decode()
+    tree = ET.fromstring(z)
+    results = tree.findall('comments/comment')
+    array = []
+    for item in results:
+        y = item.find('count').text
+        w = int(y)
+        array.append(w)
+    sum = sum(array)
+    print(sum)
+    x =x-1
